544012eed445c5e863dc0426773ef0cf38bd5664
Magdalena Perry <magperry16@gmail.com>
2022-09-01 14:59:48 -0400

    navbar and footer functionality added--react version 18 working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
+import NavBar from "./components/Navbar";
+import Footer from "./components/Footer";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <>
+          {/* <StoreProvider> */}
+          <NavBar />
+          <Routes>
+            {/* create new route here and add element to render */}
+            {/* <Route path="/" element={<NavBar />} /> */}
+          </Routes>
+          {/* add footer below routes */}
+          <Footer />
+          {/* </StoreProvider> */}
+        </>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+//
+function Footer() {
+  return (
+    <>
+      <footer className="bg-secondary footer-container">
+        <hr />
+        This is my footer
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
